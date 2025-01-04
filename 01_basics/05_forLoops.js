649d40ee3b15f7787c8768f0c19ec5f151c800e1
aryanmallick302 <aryanmallick.office@gmail.com>
2025-01-04 13:48:07 +0000

    day2

---
+++ 01_basics/05_forLoops.js
@@ -0,0 +1,75 @@
+// for loop
+
+// for(let index=0; index<=10; index++){
+//     const element = index;
+//     if(element == 5){
+//         console.log("5 is best number");        
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     // const element = array[i];
+//     console.log(`Outer loop value : ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         // const element = array[j];
+//         console.log(`Inner loop value : ${j}`);   
+        
+//     }    
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     // const element = ;
+//     console.log(`Table number : ${i}`);
+
+//     for (let j = 0; j <= 10; j++) {
+//         // const element = array[j];
+//         console.log(i + "*" + j + "=" + i*j);        
+        
+//     }       
+    
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//array loop
+// let myArray = ["flash" , "superHero", "batman" , "ironman"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+        
+// }
+// for (let index = myArray.length; index >= 0; index--) {
+//     const element = myArray[index];
+//     console.log(element);
+        
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//object loop
+// let myObject = [{
+//     name : "Aryan",
+//     email : "aryanmallick@gmail.com"
+// }, {
+//     name : "Sumit",
+//     email : "sumit@gmail.com"
+// },{
+//     name : "Srishti", 
+//     email : "srishti@gmail.com"
+// },{
+//     name : "Rupali",
+//     email : "rupali@gmail.com"
+// },{
+//     name : "Ajitabh",
+//     email : "ajitabh@gmail.com"
+// }];
+
+// for (let i = myObject.length-1; i >= 0; i--) {
+//     let name = myObject[i].name;  
+//     console.log(name);
+//     let email = myObject[i].email;
+//     console.log(email);   
+      
+// }
